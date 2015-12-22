1f84f55e3075c777e534c587d847203ed4688b7d
Justin Lawson <jaglawson3@gmail.com>
2015-12-22 13:16:51 -0700

    solution to 18-Game of Threes

---
+++ 17_CSVtoMD/solutions/justin.js
@@ -0,0 +1,23 @@
+var csv = require('../csv')
+
+var columnArray = [null];
+var firstNames = [];
+var lastNames = [];
+var addresses = [];
+
+cvsToArray function(csv){
+  csv.split('');
+}
+
+csvToAray(columnArray);
+    for (i=1; i<columnArray.length, i++){
+          if(i%3===0){
+            csvToArray[i].push(addresses);
+          }else if(i%2===0){
+            csvToArray[i].push(lastNames);
+          }else{
+            csvToArray[i].push(firstNames);
+          }
+}
+
+join()
